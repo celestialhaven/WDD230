57c9658db74e2cbe05a078416387509473076c1e
celestialhaven <jonzmabz04@gmail.com>
2022-10-30 02:15:40 +0800

    week07

---
+++ chamber/script/lazyload.js
@@ -0,0 +1,30 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  rootMargin: '0px 0px 10px 0px',
+  threshold: 1
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};  
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
