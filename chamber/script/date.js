6069a8e36e866086464202e61e63d516237a72fe
celestialhaven <jonzmabz04@gmail.com>
2022-11-11 01:49:01 +0800

    week07 update

--- chamber/script/date.js
+++ chamber/script/date.js
@@ -1,10 +1,14 @@
-try {
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-    };
-    document.getElementById("date").textContent = new Date().toLocaleDateString("en-us", options);
-} catch (e) {alert(Error);
+//Let's get the date first
+let d = new Date()
+//format the date into a UK format date
+let fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full",
+}).format(d);
+document.getElementById("date").innerHTML = fulldateUK
+
+let numVisits = Number(window.localStorage.getItem("numberOfVisits"));
+window.localStorage.setItem("numberOfVisits", numVisits+1)
+
+if (numVisits === 0) {
+    window.localStorage.setItem("firstVisit", new Date())
 }
