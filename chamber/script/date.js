c3f1bfc6988cec20cecb4740210aeb6201894c08
celestialhaven <jonzmabz04@gmail.com>
2022-11-24 10:44:58 +0800

    week07

--- chamber/script/date.js
+++ chamber/script/date.js
@@ -12,3 +12,26 @@ window.localStorage.setItem("numberOfVisits", numVisits+1)
 if (numVisits === 0) {
     window.localStorage.setItem("firstVisit", new Date())
 }
+
+//Using local storage, display the amount of time in days (rounded to a whole number) between user visits to this page by the user's agent (browser). You may elect to display this information where you deem fit on the page.
+//localStorage.clear() //=> to reset local storage
+if (localStorage.hasOwnProperty("firstVisit") == false) {
+    localStorage.setItem("firstVisit", d.getTime());
+}
+localStorage.setItem("date", d.getTime());
+
+let difference = parseInt(localStorage.getItem("current-visit")) - parseInt(localStorage.getItem("firstVisit"))
+
+if (difference < 30000) {
+    document.getElementById("date p").innerHTML = fulldateUK + '<br><p>Welcome! This is your first visit</p>'
+} else {
+
+if (43200000 < difference &&  difference < 86400000) {
+    document.getElementById("date p").innerHTML = fulldateUK + '<br><p>Welcome Back!</p>'
+}
+
+if (86400000 < difference) {
+    days = Math.round(difference/86400000)
+    document.getElementById("date p").innerHTML = fulldateUK + `<br><p>Welcome Back! It's been ${days} days since your last visit.</p>`
+}
+}
